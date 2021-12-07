7fa75e84ce9afcf9974a789e35c3686b505a6e84
seedevk8s <seedevk8s@gmail.com>
2021-12-07 23:02:58 +0900

    Initial commit

---
+++ src/com/datastructure/list/MyArrayList.java
@@ -0,0 +1,82 @@
+package com.datastructure.list;
+
+import java.util.Arrays;
+
+public class MyArrayList<T> implements IList<T> {
+    private static final int DEFAULT_SIZE = 50;
+
+    private int size;
+    private T[] elements;
+
+    public MyArrayList() {
+        this.size = 0;
+        this.elements = (T[]) new Object[DEFAULT_SIZE];
+    }
+
+    @Override
+    public void add(T t) {                              //현재 리스트의 가장 뒤에 데이터를 add함.
+        if (this.size == this.elements.length) {        //배열이 꽉차있는 경우 ==> 이경우에는 배열을 늘려줘야함 (기존 저장되어 있던걸 옮겨야함)
+            this.elements = Arrays.copyOf(this.elements, this.size * 2);  //(복사대상 엘리먼트, 사이즈를 어느정도까지 늘려줄지 설정함)
+        }
+
+        this.elements[this.size] = t;               //index 위치값을 this.size로 접근하면 항상 현재 size 크기 인덱스위치에 할당하게 됨
+        this.size += 1;
+    }
+
+    @Override
+    public void insert(int index, T t) {
+        if (this.size == this.elements.length) {        //배열이 꽉차있는 경우 ==> 이경우에는 배열을 늘려줘야함 (기존 저장되어 있던걸 옮겨야함)
+            this.elements = Arrays.copyOf(this.elements, this.size * 2);  //(복사대상 엘리먼트, 사이즈를 어느정도까지 늘려줄지 설정함)
+        }
+
+        for (int i = index; i < this.size; i++) {       //index 뒤에 있는 데이터들은 한칸씩 뒤로 밀어줘야함
+            this.elements[i+1] = this.elements[i];
+        }
+
+        this.elements[index] = t;
+        this.size++;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean delete(T t) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteByIndex(int index) {
+        return false;
+    }
+
+    @Override
+    public T get(int index) {
+        if (this.size <= index) {
+            throw new IndexOutOfBoundsException();
+        }
+        return this.elements[index];
+    }
+
+    @Override
+    public int indexOf(T t) {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(T t) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
---
+++ src/com/datastructure/list/MyArrayListTest.java
@@ -0,0 +1,19 @@
+package com.datastructure.list;
+
+import java.util.Arrays;
+
+public class MyArrayListTest {
+    public static void main(String[] args) {
+        IList<Integer> myArrayList = new MyArrayList<>();
+        myArrayList.add(1);
+        myArrayList.add(2);
+        myArrayList.add(3);
+        myArrayList.insert(2,4);
+
+        System.out.println(myArrayList.get(0));
+        System.out.println(myArrayList.get(1));
+        System.out.println(myArrayList.get(2));
+        System.out.println(myArrayList.get(3));
+
+    }
+}
